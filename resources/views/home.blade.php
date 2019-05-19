daba98470022b6b2c772e194fd826da6c9a8f0f7
Yuri <smoorlex@gmail.com>
2019-05-20 00:12:54 +0300

    new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="/css/style.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
-    <title>Document</title>
+    <title>Chill films</title>
 </head>
 <body>
         <header>
         <h1>Chill films</h1>
         <nav>
             <ul>
-                <li><a href="/sections/">Разделы</a></li>
                 <li><a href="/registration">Регистрация</a></li>
                 <li><a href="/login">Вход</a></li>
                 <li><a href="/feedback">Поддержка</a></li>
--- resources/views/part.blade.php
+++ resources/views/part.blade.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    Здесь находится видео материал
+<header>
+        <h1>Chill films</h1>
+        <nav>
+            <ul>
+                <li><a href="/sections/">Разделы</a></li>
+                <li><a href="/registration">Регистрация</a></li>
+                <li><a href="/login">Вход</a></li>
+                <li><a href="/feedback">Поддержка</a></li>
+            </ul>
+        </nav>
+        </header>
+
 </body>
 </html>
