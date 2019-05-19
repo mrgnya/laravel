212882e40b0d802a5def6983b95a76d9b56584e6
Yuri <smoorlex@gmail.com>
2019-05-19 20:14:31 +0300

    baselaravel

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/sections', function () {
+    return view('sections');
+});
+Route::get('/sections/{list}/', function ($list) {
+        return view('list');
+    });
+Route::get('/sections/{list}/{film_name}', function ($list, $film_name) {
+        return view('seasons');
+    });
+Route::get('/sections/{list}/{film_name}/{season?}', function ($list, $film_name, $season=null) {
+        return view('film');
+    });
+Route::get('/sections/{list}/{film_name}/{season?}/{part?)', function ($list, $film_name, $season, $part) {
+        return view('part');
+    });
+
